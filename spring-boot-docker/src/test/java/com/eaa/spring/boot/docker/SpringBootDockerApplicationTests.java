302316b16a71d1523adafe5fb5370bf8670862f6
eabiatallah <74223005+eabiatallah@users.noreply.github.com>
2022-01-17 22:36:45 -0500

    First Test Push

---
+++ spring-boot-docker/src/test/java/com/eaa/spring/boot/docker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaa.spring.boot.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
